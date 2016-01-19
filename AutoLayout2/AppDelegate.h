8b3e266bb6dd87d3e8cec340a9e448baf0deeef1
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-19 15:30:48 -0500

    Initial Commit

---
+++ AutoLayout2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout2
+//
+//  Created by Kerry Toonen on 2016-01-19.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
